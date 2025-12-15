c01e63472df2f9c9e6205d3ef7f99f08cc0ddfb3
Eloka Michel <elokamichel@gmail.com>
2025-12-15 11:07:21 +0100

    finish1

--- FrontBadgeauseDoor/src/config.ts
+++ FrontBadgeauseDoor/src/config.ts
@@ -4,5 +4,3 @@ export const MQTT_USERNAME_DEFAULT = import.meta.env.VITE_MQTT_USERNAME || "toto
 export const MQTT_PASSWORD_DEFAULT = import.meta.env.VITE_MQTT_PASSWORD || "toto";
 export const BADGEUSE_LINK_RADIUS = 60; // Distance max, en px, pour lier automatiquement badgeuse et porte
 export const ENTRANCE_API_URL = import.meta.env.VITE_ENTRANCE_API_URL || "http://localhost:9500/api";
-export const BADGEUSE_LINK_RADIUS = 60; // Distance max, en px, pour lier automatiquement badgeuse et porte
-export const ENTRANCE_API_URL = import.meta.env.VITE_ENTRANCE_API_URL || "http://localhost:9500/api";
